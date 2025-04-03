f1248a82e01ff832c96b3e286f33fb0f40217fe5
d1laav <dimmanuel31@gmail.com>
2025-04-03 14:46:24 +0700

    Update MainScreen fix

--- app/src/main/java/com/android/example/uts_map/ui/screen/main/MainScreen.kt
+++ app/src/main/java/com/android/example/uts_map/ui/screen/main/MainScreen.kt
@@ -13,7 +13,7 @@ import com.android.example.uts_map.ui.component.navbar.BottomNavigationBar
 fun MainScreen() {
     val navController = rememberNavController()
     val currentBackStack by navController.currentBackStackEntryAsState()
-    val currentRoute = currentBackStack?.destination?.route ?: BottomNavItem.Journey.route //"journey"
+    val currentRoute = currentBackStack?.destination?.route ?: "journey"
 
     println(">> Current route: $currentRoute")
 
@@ -27,22 +27,22 @@ fun MainScreen() {
     ) { innerPadding ->
         NavHost(
             navController = navController,
-            startDestination = BottomNavItem.Journey.route,
+            startDestination = "journey",
             modifier = Modifier.padding(innerPadding)
         ) {
-            composable(BottomNavItem.Journey.route) {
+            composable("journey") {
                 println(">> Showing Journey Screen")
                 Text("Journey Screen", modifier = Modifier.padding(16.dp))
             }
-            composable(BottomNavItem.Calendar.route) {
+            composable("calendar") {
                 println(">> Showing Calendar Screen")
                 Text("Calendar Screen", modifier = Modifier.padding(16.dp))
             }
-            composable(BottomNavItem.Media.route) {
+            composable("media") {
                 println(">> Showing Media Screen")
                 Text("Media Screen", modifier = Modifier.padding(16.dp))
             }
-            composable(BottomNavItem.Atlas.route) {
+            composable("atlas") {
                 println(">> Showing Atlas Screen")
                 Text("Atlas Screen", modifier = Modifier.padding(16.dp))
             }
