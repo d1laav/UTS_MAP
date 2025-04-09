3b84c466baf3dcdbb3a26f59f8496a940db29b80
aurelius-devon <aurelius.dvn@gmail.com>
2025-04-09 19:28:19 +0700

    update tablet

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,4 +63,6 @@ dependencies {
     implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation("com.maxkeppeler.sheets-compose-dialogs:calendar:1.1.0")
     implementation("androidx.compose.material3:material3:1.1.0")
+    implementation ("androidx.compose.material:material-icons-extended:<version>")
+
 }
